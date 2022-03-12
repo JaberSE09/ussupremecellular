09037f65dcb0a0ef69d5aff1df82efcffb5f7e51
Sajid Jaber <sajidjaber@yahoo.com>
2022-03-11 19:07:36 -0600

    fixed about us

--- biztrox/themes/biztrox/js/script.js
+++ biztrox/themes/biztrox/js/script.js
@@ -5,6 +5,16 @@
  * GITHUB: https://github.com/themefisher/
  */
 
+ function formDisplay() {
+  var x = document.getElementById("form");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
 (function ($) {
   'use strict';
 
